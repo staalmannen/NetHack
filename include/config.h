650c519bf4a3fd1d82ee93e076763ca92f7011fd
Jens Staal <staal1978@gmail.com>
2023-07-06 09:08:09 +0200

    undo some hacks, still bitfield problems in include/objects.h

--- include/config.h
+++ include/config.h
@@ -475,7 +475,7 @@ typedef unsigned char uchar;
  * allocate a separate character for each bitfield.  (The bitfields used never
  * have more than 7 bits, and most are only 1 bit.)
  */
-#define BITFIELDS /* Good bitfield handling */
+//#define BITFIELDS /* Good bitfield handling */
 
 /* #define STRNCMPI */ /* compiler/library has the strncmpi function */
 
